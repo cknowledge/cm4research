27249247b1c4bbd2a360b08ebfc3bc611e9b3fd4
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-04-21 15:00:20 +0200

    improve cm demo

--- script/test-mlperf-loadgen-py/python/main.py
+++ script/test-mlperf-loadgen-py/python/main.py
@@ -9,7 +9,7 @@ Testing MLPerf loadgen
 import numpy as np
 
 import cmind.utils
-cmind.utils.debug_here(__file__, port=5678, text='Debugging loadgen!', env_debug_uid='a83d9c76755a40af')
+cmind.utils.debug_here(__file__, port=5678, text='Debugging loadgen!', env_debug_uid='a83d9c76755a40af').breakpoint()
 
 print ('- Importing MLPerf loadgen library ...')
 import mlperf_loadgen as lg
