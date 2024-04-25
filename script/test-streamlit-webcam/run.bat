a4224755eb4803c6c462c040112f3ed5ceb848e3
Grigori Fursin <Grigori.Fursin@cTuning.org>
2024-04-25 13:47:35 +0200

    clean up

--- script/test-streamlit-webcam/run.bat
+++
@@ -1,6 +0,0 @@
-echo ========================================================
-
-streamlit run %CM_TMP_CURRENT_SCRIPT_PATH%\src\app.py
-IF %ERRORLEVEL% NEQ 0 EXIT %ERRORLEVEL%
-
-echo ========================================================
--- script/test-streamlit-webcam/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "========================================================"
-
-streamlit run ${CM_TMP_CURRENT_SCRIPT_PATH}/src/app.py
-test $? -eq 0 || exit 1
-
-echo ========================================================
